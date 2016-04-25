24052482097eeaf33b70986427549f4aabacf452
Cobalto-sfx <santi.sfx@gmail.com>
2016-04-25 14:22:49 +0200

    Añadido boton de Facebook y parte de la API. Contiene errores, consultar la documentación.

--- app/src/main/java/ucm/fdi/adventurerol/MainMenu.java
+++ app/src/main/java/ucm/fdi/adventurerol/MainMenu.java
@@ -2,6 +2,23 @@ package ucm.fdi.adventurerol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+/*
+Web con la documentación de la API de Facebook
+https://developers.facebook.com/docs/facebook-login/android
+
+ */
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,4 +27,50 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+    CallbackManager callbackManager;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.usersettings_fragment_login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.splash, container, false);
+
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        CallbackManager callbackManager = null;
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+        //Por corregir
+
+        return view;
+    }
+
 }
