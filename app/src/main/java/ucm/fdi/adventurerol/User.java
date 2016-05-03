6c77124e2fb4ba5ee2c7d0f60a4789fbf4fe2d2d
Cobalto-sfx <santi.sfx@gmail.com>
2016-05-03 13:40:31 +0200

    Añadidas las preferencias compartidas como método de almacenamiento para el login con Facebook. Tambien se ha implementado nuevas clases y metodos para la gestion del Inicio de sesión con Facebook.

---
+++ app/src/main/java/ucm/fdi/adventurerol/User.java
@@ -0,0 +1,14 @@
+package ucm.fdi.adventurerol;
+
+/**
+ * Created by santi on 3/5/16.
+ */
+
+public class User {
+
+    public String name;
+    public String email;
+    public String facebookID;
+    public String gender;
+
+}
